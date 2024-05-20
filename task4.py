55d4c98e44ab3c8923baba24ebc921acc38bf72a
PirozhkovaElizaveta <zhenshina21@mail.ru>
2024-05-20 16:44:59 +0300

    add task4

---
+++ task4.py
@@ -0,0 +1,19 @@
+# class SmartHouse():
+#     def turn_on_light(self):
+#         pass
+#     def heat_food(self):
+#         pass
+#     def turn_on_music(self):
+#         pass
+
+class Light():
+    def turn_on_light(self):
+        print("Свет включен")
+
+class Food():
+    def heat_food(self):
+        print("Еда начала разогреваться")
+
+class Music():
+    def turn_on_music(self):
+        print("Вот, что мне удалось найти на Яндекс музыке")
