5403ed0e5d0a658946b78a61e3183a954093ae43
PirozhkovaElizaveta <zhenshina21@mail.ru>
2024-05-20 16:30:27 +0300

    add task2

---
+++ task2.py
@@ -0,0 +1,36 @@
+# class Report():
+#     def __init__(self, title, content):
+#         self.tittle = title
+#         self.content = content
+#
+#     def DocPrinter(self):
+#         print(f"Сформирован отчет - {self.title} - {self.content}")
+
+from abc import ABC, abstractmethod
+
+class Formatted(ABC):
+    @abstractmethod
+    def format(self, report):
+        pass
+
+class TextFormatted(Formatted):
+    def format(self, report):
+        print(report.title)
+        print(report.content)
+
+class HtmlFormatted(Formatted):
+    def format(self, report):
+        print(f"<h1>{report.title}</h1>")
+        print(f"<p>{report.content}</p>")
+
+class Report():
+    def __init__(self, title, content, formatted):
+        self.title = title
+        self.content = content
+        self.formatted = formatted
+
+    def docPrinter(self):
+        self.formatted.format(self)
+
+report = Report('заголовок отчета', 'текст отчета', HtmlFormatted())
+report.docPrinter()
